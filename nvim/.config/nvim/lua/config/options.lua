52e10b5cd2a7437b75d9ab52a18177c32046b369
直里 <zhenli@lesspro.local>
2023-09-05 14:58:19 +0800

    [Feat] [nvim] delete lazyvim config

--- nvim/.config/nvim/lua/config/options.lua
+++
@@ -1,7 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-vim.opt.winbar = "%f%m"
--- vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "", linehl = "", numhl = "" })
--- vim.fn.sign_define("DapBreakpointRejected", { text = "", texthl = "", linehl = "", numhl = "" })
--- vim.fn.sign_define("DapStopped", { text = "", texthl = "", linehl = "", numhl = "" })
