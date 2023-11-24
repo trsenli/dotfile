ee30ffd6308b7bbb5ed4bb7c5a46f89054c07530
lzl <ztruth@qq.com>
2023-11-24 18:28:00 +0800

    feat(nvim,ui): some customization folloing devaslife

---
+++ nvim/.config/nvim/lua/config/treesitter.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"cmake",
+				"cpp",
+				"css",
+				"gitignore",
+				"go",
+				"http",
+        "python"
+			},
+    },
+  }
+}
