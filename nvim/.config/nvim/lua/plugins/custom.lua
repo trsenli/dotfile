be0fe7f7c306c10f56cb270344b24f991f060206
lzl <ztruth@qq.com>
2023-11-24 10:19:34 +0800

    feat(ui): add cursor animation and custom logo

--- nvim/.config/nvim/lua/plugins/custom.lua
+++ nvim/.config/nvim/lua/plugins/custom.lua
@@ -27,5 +27,34 @@ return{
         opts = { skip = true },
       })
     end,
-  }
+  },
+  -- UI animations
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = function(_,opts)
+      opts.scroll = {
+        enable = false,
+      }
+    end,
+  },
+  -- logo
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    opts = function (_,opts)
+      local logo = [[
+     ████████╗██████╗ ███████╗███████╗███╗   ██╗██╗     ██╗
+     ╚══██╔══╝██╔══██╗██╔════╝██╔════╝████╗  ██║██║     ██║
+        ██║   ██████╔╝███████╗█████╗  ██╔██╗ ██║██║     ██║
+        ██║   ██╔══██╗╚════██║██╔══╝  ██║╚██╗██║██║     ██║
+        ██║   ██║  ██║███████║███████╗██║ ╚████║███████╗██║
+        ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝
+      ]]
+      logo = string.rep("\n",8) .. logo .. "\n\n"
+      opts.config.header = vim.split(logo, "\n")
+    end,
+  },
 }
+
+
